a561df4822cf5e4b206e1e0d315ca6b64c29b390
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-01-09 17:06:38 +0200

    Add DS exercise - "Linear Data Structures - Lists - Lab"

---
+++ Data Structures/Exercises/01. Lists - Lab/Linear-Data-Structures/Lists/ArrayList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    public int Count { get; private set; }
+
+    private T[] _array;
+
+    public ArrayList()
+    {
+        _array = new T[2];
+        Count = 0;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return _array[index];
+        }
+
+        set
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            _array[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (_array.Length <= Count)
+        {
+            var newArr = new T[Count * 2];
+            Array.Copy(_array, newArr, Count);
+            _array = newArr;
+        }
+
+        _array[Count] = item;
+
+        Count++;
+    }
+
+    public T RemoveAt(int index)
+    {
+        if (index < 0 || index >= Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        var element = _array[index];
+
+        Count--;
+
+        for (int i = index; i < Count; i++)
+        {
+            _array[i] = _array[i + 1];
+        }
+
+        return element;
+    }
+}
