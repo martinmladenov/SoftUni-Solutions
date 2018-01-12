690b35aafed162cdaf7f1a82dd1a6e7bde6dc752
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-01-12 21:59:53 +0200

    Add DS exercise - "Stacks and Queues - Lab"

---
+++ Data Structures/Exercises/02. Stacks and Queues - Lab/Circular-Queue/CircularQueue/CircularQueue.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+public class CircularQueue<T>
+{
+    private T[] _arr;
+    private int _readIndex = 0;
+    private int _writeIndex = 0;
+
+
+    private const int DefaultCapacity = 4;
+
+    public int Count { get; private set; }
+
+    public CircularQueue(int capacity = DefaultCapacity)
+    {
+        _arr = new T[capacity];
+    }
+
+    public void Enqueue(T element)
+    {
+        Count++;
+        _arr[_writeIndex] = element;
+
+        _writeIndex = (_writeIndex + 1) % _arr.Length;
+
+        if (_writeIndex == _readIndex)
+        {
+            GrowArray();
+        }
+    }
+
+    private void GrowArray()
+    {
+        var newArr = new T[_arr.Length * 2];
+        for (int i = 0; i < _arr.Length; i++)
+        {
+            newArr[i] = _arr[(_readIndex + i) % _arr.Length];
+        }
+
+        _readIndex = 0;
+        _writeIndex = _arr.Length;
+        _arr = newArr;
+    }
+
+    // Should throw InvalidOperationException if the queue is empty
+    public T Dequeue()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        Count--;
+        T element = _arr[_readIndex];
+        _readIndex = (_readIndex + 1) % _arr.Length;
+        return element;
+    }
+
+    public T[] ToArray()
+    {
+        int length;
+        if (_writeIndex >= _readIndex)
+        {
+            length = _writeIndex - _readIndex;
+        }
+        else
+        {
+            length = _arr.Length - _writeIndex + _readIndex;
+        }
+
+        var newArr = new T[length];
+        for (int index = _readIndex; index != _writeIndex; index = (index + 1) % _arr.Length)
+        {
+            newArr[index] = _arr[index];
+        }
+
+        return newArr;
+    }
+}
+
+
+public class Example
+{
+    public static void Main()
+    {
+
+        CircularQueue<int> queue = new CircularQueue<int>();
+
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        queue.Enqueue(5);
+        queue.Enqueue(6);
+
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        int first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-7);
+        queue.Enqueue(-8);
+        queue.Enqueue(-9);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-10);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+    }
+}
---
+++ Data Structures/Exercises/02. Stacks and Queues - Lab/Linked-List/LinkedList/LinkedList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedList<T> : IEnumerable<T>
+{
+    public int Count { get; private set; }
+
+    private Node<T> _head;
+    private Node<T> _tail;
+
+    public void AddFirst(T item)
+    {
+        var newNode = new Node<T>(item) { Next = _head };
+        if (Count == 0)
+        {
+            _tail = newNode;
+        }
+
+        _head = newNode;
+        Count++;
+    }
+
+    public void AddLast(T item)
+    {
+        var newNode = new Node<T>(item);
+
+        if (Count == 0)
+        {
+            _head = newNode;
+        }
+        else
+        {
+            _tail.Next = newNode;
+        }
+        _tail = newNode;
+        Count++;
+    }
+
+    public T RemoveFirst()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+        var oldHead = _head;
+
+        _head = oldHead.Next;
+        Count--;
+
+        if (Count == 0)
+        {
+            _tail = null;
+        }
+
+        return oldHead.Value;
+    }
+
+    public T RemoveLast()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        Count--;
+
+        var oldTail = _tail;
+
+        if (Count == 0)
+        {
+            _head = null;
+            _tail = null;
+        }
+        else
+        {
+            var current = _head;
+            while (current.Next != _tail)
+            {
+                current = current.Next;
+            }
+            current.Next = null;
+            _tail = current;
+        }
+        return oldTail.Value;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var current = _head;
+        while (current != null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Node<T>
+{
+    public T Value { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+    }
+}
