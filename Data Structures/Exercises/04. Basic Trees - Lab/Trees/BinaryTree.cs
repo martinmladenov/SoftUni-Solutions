80e7ddac0c6201939f214a1e33a75a10408c879a
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-01-19 23:34:29 +0200

    Add DS exercise - "Basic Tree Data Structures - Lab"

---
+++ Data Structures/Exercises/04. Basic Trees - Lab/Trees/BinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// ReSharper disable CheckNamespace
+public class BinaryTree<T>
+{
+    public T Value { get; }
+    public BinaryTree<T> LeftChild { get; set; }
+    public BinaryTree<T> RightChild { get; set; }
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        Value = value;
+        LeftChild = leftChild;
+        RightChild = rightChild;
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent * 2) + Value);
+
+        LeftChild?.PrintIndentedPreOrder(indent + 1);
+        RightChild?.PrintIndentedPreOrder(indent + 1);
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        LeftChild?.EachInOrder(action);
+        action(Value);
+        RightChild?.EachInOrder(action);
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        LeftChild?.EachPostOrder(action);
+        RightChild?.EachPostOrder(action);
+        action(Value);
+    }
+}
---
+++ Data Structures/Exercises/04. Basic Trees - Lab/Trees/Tree.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+public class Tree<T>
+{
+    public T Value { get; set; }
+    public IList<Tree<T>> Children { get; set; }
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        Value = value;
+        Children = new List<Tree<T>>(children);
+    }
+
+    public void Print(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent * 2) + Value);
+
+        foreach (var child in Children)
+        {
+            child.Print(indent + 1);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(Value);
+
+        foreach (var child in Children)
+        {
+            child.Each(action);
+        }
+    }
+
+    public IEnumerable<T> OrderDFS()
+    {
+        var list = new List<T>();
+        DFSAdd(list);
+        return list;
+    }
+
+    private void DFSAdd(List<T> list)
+    {
+        foreach (var child in Children)
+        {
+            child.DFSAdd(list);
+        }
+
+        list.Add(Value);
+    }
+
+    public IEnumerable<T> OrderBFS()
+    {
+        var list = new List<T>();
+        var queue = new Queue<Tree<T>>(new[] { this });
+
+        while (queue.Count > 0)
+        {
+            list.Add(queue.Peek().Value);
+
+            foreach (var child in queue.Dequeue().Children)
+            {
+                queue.Enqueue(child);
+            }
+        }
+
+        return list;
+    }
+}
