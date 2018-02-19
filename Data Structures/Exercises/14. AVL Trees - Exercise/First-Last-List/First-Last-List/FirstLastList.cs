0465c5c10ad5b2fed5754e6deb1ab7773120fa49
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-02-19 11:22:10 +0200

    Add DS exercise - First Last List

---
+++ Data Structures/Exercises/14. AVL Trees - Exercise/First-Last-List/First-Last-List/FirstLastList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class FirstLastList<T> : IFirstLastList<T> where T : IComparable<T>
+{
+    private LinkedList<T> byInsertionOrder;
+    private OrderedBag<LinkedListNode<T>> byValue;
+    private OrderedBag<LinkedListNode<T>> byValueReversed;
+
+    public FirstLastList()
+    {
+        byInsertionOrder = new LinkedList<T>();
+        byValue = new OrderedBag<LinkedListNode<T>>((x, y) => x.Value.CompareTo(y.Value));
+        byValueReversed = new OrderedBag<LinkedListNode<T>>((x, y) => y.Value.CompareTo(x.Value));
+    }
+
+    public int Count => byInsertionOrder.Count;
+
+    public void Add(T element)
+    {
+        byInsertionOrder.AddLast(element);
+        byValue.Add(byInsertionOrder.Last);
+        byValueReversed.Add(byInsertionOrder.Last);
+    }
+
+    public void Clear()
+    {
+        byInsertionOrder.Clear();
+        byValue.Clear();
+        byValueReversed.Clear();
+    }
+
+    public IEnumerable<T> First(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        LinkedListNode<T> current = byInsertionOrder.First;
+
+        for (int i = 0; i < count; i++)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+
+    public IEnumerable<T> Last(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        LinkedListNode<T> current = byInsertionOrder.Last;
+
+        for (int i = 0; i < count; i++)
+        {
+            yield return current.Value;
+            current = current.Previous;
+        }
+    }
+
+    public IEnumerable<T> Max(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return byValueReversed.Take(count).Select(node => node.Value);
+    }
+
+    public IEnumerable<T> Min(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return byValue.Take(count).Select(node => node.Value);
+    }
+
+    public int RemoveAll(T element)
+    {
+        var node = new LinkedListNode<T>(element);
+        foreach (var toRemove in byValue.Range(node, true, node, true))
+        {
+            byInsertionOrder.Remove(toRemove);
+        }
+
+        byValueReversed.RemoveAllCopies(node);
+        return byValue.RemoveAllCopies(node);
+    }
+}
---
+++ Data Structures/Exercises/14. AVL Trees - Exercise/First-Last-List/First-Last-List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        OrderedBag<int> ob = new OrderedBag<int>();
+        ob.Add(7);
+        ob.Add(5);
+        ob.Add(5);
+        ob.Add(5);
+        ob.Add(1);
+
+        Console.WriteLine(ob.Reversed());
+    }
+}
