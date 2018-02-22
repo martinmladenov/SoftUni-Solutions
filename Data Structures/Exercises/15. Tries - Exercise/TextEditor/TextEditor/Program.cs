65086ddeda3e6466f773f134bef7acbb1971febf
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-02-22 22:41:58 +0200

    Add DS exercise - TextEditor

---
+++ Data Structures/Exercises/15. Tries - Exercise/TextEditor/TextEditor/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        TextEditor textEditor = new TextEditor();
+
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            var split = input.Split();
+            string command = split[0];
+            if (command == "login")
+            {
+                textEditor.Login(split[1]);
+            }
+            else if (command == "logout")
+            {
+                textEditor.Logout(split[1]);
+            }
+            else if (command == "users")
+            {
+                string prefix = string.Empty;
+                if (split.Length > 1)
+                {
+                    prefix = split[1];
+                }
+
+                Console.WriteLine(string.Join(Environment.NewLine, textEditor.Users(prefix)));
+            }
+            else
+            {
+                string username = command;
+                command = split[1];
+
+                if (command == "insert")
+                {
+                    int index = int.Parse(split[2]);
+                    string toInsert = GetText(split, 3);
+                    textEditor.Insert(username, index, toInsert);
+                }
+                else if (command == "prepend")
+                {
+                    string toPrepend = GetText(split, 2);
+                    textEditor.Prepend(username, toPrepend);
+                }
+                else if (command == "delete")
+                {
+                    int index = int.Parse(split[2]);
+                    int length = int.Parse(split[3]);
+                    textEditor.Delete(username, index, length);
+                }
+                else if (command == "substring")
+                {
+                    int index = int.Parse(split[2]);
+                    int length = int.Parse(split[3]);
+                    textEditor.Substring(username, index, length);
+                }
+                else if (command == "clear")
+                {
+                    textEditor.Clear(username);
+                }
+                else if (command == "length")
+                {
+                    Console.WriteLine(textEditor.Length(username));
+                }
+                else if (command == "print")
+                {
+                    Console.WriteLine(textEditor.Print(username));
+                }
+                else if (command == "undo")
+                {
+                    textEditor.Undo(username);
+                }
+            }
+        }
+    }
+
+    private static string GetText(string[] split, int index)
+    {
+        string str = string.Join(" ", split.Skip(index));
+        return str.Substring(1, str.Length - 2);
+    }
+}
---
+++ Data Structures/Exercises/15. Tries - Exercise/TextEditor/TextEditor/TextEditor.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+public class TextEditor : ITextEditor
+{
+
+    Trie<StringBuilder> userTexts;
+    Trie<Stack<string>> userHistory;
+
+
+    public TextEditor()
+    {
+        userTexts = new Trie<StringBuilder>();
+        userHistory = new Trie<Stack<string>>();
+    }
+
+    public void Login(string username)
+    {
+        userTexts.Insert(username, new StringBuilder());
+        if (!userHistory.Contains(username))
+            userHistory.Insert(username, new Stack<string>());
+    }
+
+    public void Logout(string username)
+    {
+        userTexts.Delete(username);
+        userHistory.Delete(username);
+    }
+
+    public void Prepend(string username, string str)
+    {
+        this.Insert(username, 0, str);
+    }
+
+    public void Insert(string username, int index, string str)
+    {
+        if (!userTexts.Contains(username))
+        {
+            return;
+        }
+
+        this.PushToHistory(username);
+
+        userTexts.GetValue(username).Insert(index, str);
+    }
+
+    public void Substring(string username, int startIndex, int length)
+    {
+        if (!userTexts.Contains(username))
+        {
+            return;
+        }
+
+        this.PushToHistory(username);
+
+        var sb = userTexts.GetValue(username);
+        sb.Remove(0, startIndex);
+        sb.Remove(length, sb.Length - length);
+    }
+
+    public void Delete(string username, int startIndex, int length)
+    {
+        if (!userTexts.Contains(username))
+        {
+            return;
+        }
+
+        this.PushToHistory(username);
+
+        var sb = userTexts.GetValue(username);
+        sb.Remove(startIndex, length);
+    }
+
+    public void Clear(string username)
+    {
+        if (!userTexts.Contains(username))
+        {
+            return;
+        }
+
+        this.PushToHistory(username);
+
+        userTexts.Insert(username, new StringBuilder());
+    }
+
+    public int Length(string username)
+    {
+        if (!userTexts.Contains(username))
+        {
+            return 0;
+        }
+
+        return userTexts.GetValue(username).Length;
+    }
+
+    public string Print(string username)
+    {
+        if (!userTexts.Contains(username))
+        {
+            return string.Empty;
+        }
+
+        return userTexts.GetValue(username).ToString();
+    }
+
+    public void Undo(string username)
+    {
+        //Environment.Exit(0);
+        if (!userTexts.Contains(username))
+        {
+            return;
+        }
+
+        var history = userHistory.GetValue(username);
+
+        if (history.Count == 0)
+        {
+            return;
+        }
+
+        userTexts.Insert(username, new StringBuilder(history.Pop()));
+    }
+
+    public IEnumerable<string> Users(string prefix = "")
+    {
+        return userTexts.GetByPrefix(prefix);
+    }
+
+    private void PushToHistory(string user)
+    {
+        userHistory.GetValue(user).Push(userTexts.GetValue(user).ToString());
+    }
+}
+
