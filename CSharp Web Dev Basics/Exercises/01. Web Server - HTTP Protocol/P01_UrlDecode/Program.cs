68b3a700c87afc01f2ef2e9b771f36cee7e9332b
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-09-22 11:07:11 +0300

    Add CWDB exercise - "Web Server - HTTP Protocol"

---
+++ CSharp Web Dev Basics/Exercises/01. Web Server - HTTP Protocol/P01_UrlDecode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace P01_UrlDecode
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string encodedUrl = Console.ReadLine();
+            Console.WriteLine(WebUtility.UrlDecode(encodedUrl));
+        }
+    }
+}
