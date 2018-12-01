dc72c2862b4d2f39cc845fdab1eb09155ed7896d
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-12-01 11:14:26 +0200

    Seed events

--- ASP.NET Core/Eventures/Eventures.Web/Middlewares/SeedMiddleware.cs
+++ ASP.NET Core/Eventures/Eventures.Web/Middlewares/SeedMiddleware.cs
@@ -8,6 +8,7 @@ namespace Eventures.Web.Middlewares
     using Infrastructure;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
 
     public class SeedMiddleware
     {
@@ -30,12 +31,42 @@ namespace Eventures.Web.Middlewares
             if (!this.executed)
             {
                 this.executed = true;
-                
+
                 dbContext.Database.EnsureCreated();
 
                 if (!await roleManager.RoleExistsAsync(GlobalConstants.AdminRoleName))
                 {
                     await roleManager.CreateAsync(new IdentityRole(GlobalConstants.AdminRoleName));
+                }
+
+                if (await dbContext.Events.CountAsync() < 35)
+                {
+                    var currTime = DateTime.Now;
+
+                    var random = new Random();
+
+
+                    var events = new EventuresEvent[35];
+
+                    for (int i = 0; i < 35; i++)
+                    {
+                        var startDate = currTime.AddDays(random.NextDouble() * 300 - 150);
+                        var endDate = startDate.AddDays(random.NextDouble() * 15 + 5);
+
+                        events[i] = new EventuresEvent
+                        {
+                            Name = "Seeded Event " + (i + 1),
+                            Place = "Seeded Place " + (i % 10 + 1),
+                            StartDate = startDate,
+                            EndDate = endDate,
+                            TotalTickets = random.Next(5, 500),
+                            PricePerTicket = (decimal) random.NextDouble() * 150 + 50,
+                        };
+                    }
+
+                    await dbContext.Events.AddRangeAsync(events);
+
+                    await dbContext.SaveChangesAsync();
                 }
 
                 if (!userManager.Users.Any(u => u.UserName == "admin"))
